325db388759d0cfa7169162f3fdddbbfe1773fb5
aw <aw@mega.co.nz>
2018-09-10 17:38:38 +1200

    new method of draw qrcode.

--- app/src/main/java/com/example/aw/animation/QRCodeActivity.java
+++ app/src/main/java/com/example/aw/animation/QRCodeActivity.java
@@ -41,53 +41,85 @@ public class QRCodeActivity extends Activity {
         qrImageView3 = findViewById(R.id.qr_2fa_3);
         try {
 //            gen1();
-            gen2();
+//            gen2();
             gen3();
         } catch (WriterException e) {
             e.printStackTrace();
         }
     }
     
-//    public static final String URL = "otpauth://totp/Ash:aw+1@ashtray.nz?secret=krqsus6fzz5evpidmqyozxwajfmfnqsgescvmh2eaykqcbxousuq&issuer=Ash";
-    public static final String URL = "otpauth://totp/Ash:aw+1@ashtray.nz?secret=krqsus6fzz5evpidmqyozxwajfmfnqsgescvmh2eaykqcbxousuq&issuer=Ashotpauth://totp/Ash:aw+1@ashtray.nz?secret=krqsus6fzz5evpidmqyozxwajfmfnqsgescvmh2eaykqcbxousuq&issuer=Ashotpauth://totp/Ash:aw+1@ashtray.nz?secret=krqsus6fzz5evpidmqyozxwajfmfnqsgescvmh2eaykqcbxousuq&issuer=Ashotpauth://totp/Ash:aw+1@ashtray.nz?secret=krqsus6fzz5evpidmqyozxwajfmfnqsgescvmh2eaykqcbxousuq&issuer=Ash";
+    public static final String URL = "otpauth://totp/Ash:aghghhhkkjkjkkl;lkklkl+1@ashtray.nz?secret=krqsus6fzz5evpidmqyozxwajfmfnqsgescvmh2eaykqcbxousuq&issuer=Ash";
     
     public static final int CL = URL.length();
     
     public static final int L = 700;
     
-    public static final int QR_L = (CL / 5) < 55 ? 55 : (CL / 5);
+    public static final int QR_L = 55;
     
-//    public static final float SCALE = (CL / 60) <= 1 ? 11 : (CL / 60);
-    public static final float SCALE = (L / QR_L );
+    public static final float SCALE = ((float)L / (float)QR_L);
     
     public static final float RADIUS = 5f;
     
+    private int x0 = -1, y0 = -1;
+    
     private void gen3() throws WriterException {
+        long b = System.currentTimeMillis();
         log("content length:" + CL);
         log("qr length:" + QR_L);
         log("scale:" + SCALE);
-        float resize = 8.33333f;
+        float resize = SCALE;
         
         BitMatrix bitMatrix = new MultiFormatWriter().encode(URL,BarcodeFormat.QR_CODE,QR_L,QR_L,null);
         Bitmap qr = Bitmap.createBitmap(L,L,Bitmap.Config.ARGB_8888);
         Canvas c = new Canvas(qr);
         Paint paint = new Paint();
         paint.setAntiAlias(true);
-        paint.setColor(BLACK);
-        c.drawRect(0,0,L,L,paint);
         
+        paint.setColor(BLACK);
         for (int y = 0;y < QR_L;y++) {
             for (int x = 0;x < QR_L;x++) {
                 if (bitMatrix.get(x,y)) {
-                    paint.setColor(RED);
-                } else {
-                    paint.setColor(WHITE);
+//                    log(x +"/" + y);
+                    if (x0 == -1 && y0 == -1) {
+                        x0 = x;
+                        y0 = y;
+                    }
+                    c.drawCircle(x * resize,y * resize,RADIUS,paint);
                 }
-                c.drawCircle(x * resize,y * resize,RADIUS,paint);
             }
         }
-//        drawRect(resize,c,paint);
+        drawRect(resize,c,paint);
         qrImageView1.setImageBitmap(qr);
+        log(x0 + "/" + y0);
+        log("gen3:" + (System.currentTimeMillis() - b));
+    }
+    
+    private void drawRect(float resize,Canvas c,Paint paint) {
+        paint.setColor(BLACK);
+        if (Build.VERSION.SDK_INT >= 21) {
+            c.drawRoundRect((x0 - 1) * resize,(y0 - 1) * resize,(x0 + 7) * resize,(y0 + 7) * resize,30,30,paint);
+            c.drawRoundRect(40f * resize,(y0 - 1) * resize,48f * resize,(y0 + 7) * resize,30,30,paint);
+            c.drawRoundRect((x0 - 1) * resize,(40f) * resize,(x0 + 7) * resize,48f * resize,30,30,paint);
+            
+            paint.setColor(WHITE);
+            c.drawRoundRect((x0) * resize,(y0) * resize,(x0 + 6) * resize,(y0 + 6) * resize,25,25,paint);
+            c.drawRoundRect(41f * resize,(y0) * resize,47f * resize,(y0 + 6) * resize,25,25,paint);
+            c.drawRoundRect((x0) * resize,(41f) * resize,(x0 + 6) * resize,(47f) * resize,25,25,paint);
+        } else {
+            c.drawRoundRect(new RectF((x0 - 1) * resize,(y0 - 1) * resize,(x0 + 7) * resize,(y0 + 7) * resize),30,30,paint);
+            c.drawRoundRect(new RectF(40f * resize,6f * resize,48f * resize,14f * resize),30,30,paint);
+            c.drawRoundRect(new RectF((x0 - 1) * resize,(40f) * resize,(x0 + 7) * resize,48f * resize),30,30,paint);
+            
+            paint.setColor(WHITE);
+            c.drawRoundRect(new RectF((x0) * resize,(y0) * resize,(x0 + 6) * resize,(y0 + 6) * resize),25,25,paint);
+            c.drawRoundRect(new RectF(41f * resize,(y0) * resize,47f * resize,(y0 + 6) * resize),25,25,paint);
+            c.drawRoundRect(new RectF((x0) * resize,(41f) * resize,(x0 + 6) * resize,(47f) * resize),25,25,paint);
+        }
+        
+        paint.setColor(BLACK);
+        c.drawCircle((x0 + 3) * resize,(y0 + 3) * resize,1.5f * resize,paint);
+        c.drawCircle(44f * resize,(y0 + 3) * resize,1.5f * resize,paint);
+        c.drawCircle((x0 + 3) * resize,44f * resize,1.5f * resize,paint);
     }
     
     private Map<EncodeHintType, ErrorCorrectionLevel> getHints() {
@@ -97,6 +129,7 @@ public class QRCodeActivity extends Activity {
     }
     
     private void gen2() throws WriterException {
+        long b = System.currentTimeMillis();
         BitMatrix bitMatrix = new MultiFormatWriter().encode(URL,BarcodeFormat.QR_CODE,QR_L,QR_L,null);
         int w = bitMatrix.getWidth();
         int h = bitMatrix.getHeight();
@@ -121,40 +154,7 @@ public class QRCodeActivity extends Activity {
         }
 //        drawRect(resize,c,paint);
         qrImageView2.setImageBitmap(qr);
-    }
-    
-    private void drawRect(float resize,Canvas c,Paint paint) {
-        paint.setColor(WHITE);
-        c.drawRect(3 * resize,3 * resize,11.5f * resize,11.5f * resize,paint);
-        c.drawRect(36.5f * resize,3 * resize,45 * resize,11.5f * resize,paint);
-        c.drawRect(3 * resize,36.5f * resize,11.5f * resize,45 * resize,paint);
-        
-        paint.setColor(BLACK);
-        
-        if (Build.VERSION.SDK_INT >= 21) {
-            c.drawRoundRect(3.75f * resize,3.75f * resize,10.75f * resize,10.75f * resize,30,30,paint);
-            c.drawRoundRect(37.25f * resize,3.75f * resize,44.25f * resize,10.75f * resize,30,30,paint);
-            c.drawRoundRect(3.75f * resize,37.25f * resize,10.75f * resize,44.25f * resize,30,30,paint);
-            
-            paint.setColor(WHITE);
-            c.drawRoundRect(4.75f * resize,4.75f * resize,9.75f * resize,9.75f * resize,25,25,paint);
-            c.drawRoundRect(38.25f * resize,4.75f * resize,43.25f * resize,9.75f * resize,25,25,paint);
-            c.drawRoundRect(4.75f * resize,38.25f * resize,9.75f * resize,43.25f * resize,25,25,paint);
-        } else {
-            c.drawRoundRect(new RectF(3.75f * resize,3.75f * resize,10.75f * resize,10.75f * resize),30,30,paint);
-            c.drawRoundRect(new RectF(37.25f * resize,3.75f * resize,44.25f * resize,10.75f * resize),30,30,paint);
-            c.drawRoundRect(new RectF(3.75f * resize,37.25f * resize,10.75f * resize,44.25f * resize),30,30,paint);
-            
-            paint.setColor(WHITE);
-            c.drawRoundRect(new RectF(4.75f * resize,4.75f * resize,9.75f * resize,9.75f * resize),25,25,paint);
-            c.drawRoundRect(new RectF(38.25f * resize,4.75f * resize,43.25f * resize,9.75f * resize),25,25,paint);
-            c.drawRoundRect(new RectF(4.75f * resize,38.25f * resize,9.75f * resize,43.25f * resize),25,25,paint);
-        }
-        
-        paint.setColor(BLACK);
-        c.drawCircle(7.25f * resize,7.25f * resize,17.5f,paint);
-        c.drawCircle(40.75f * resize,7.25f * resize,17.5f,paint);
-        c.drawCircle(7.25f * resize,40.75f * resize,17.5f,paint);
+        log("gen2:" + (System.currentTimeMillis() - b));
     }
     
     private void gen1() throws WriterException {
@@ -170,7 +170,7 @@ public class QRCodeActivity extends Activity {
             }
         }
         Bitmap bitmap = Bitmap.createBitmap(pixels,0,QR_L,QR_L,QR_L,Bitmap.Config.RGB_565);
-        qrImageView1.setImageBitmap(bitmap);
+        qrImageView3.setImageBitmap(bitmap);
     }
     
     private void log(Object anything) {
