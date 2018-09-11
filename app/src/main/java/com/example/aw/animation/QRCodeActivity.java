2d02e7513b90e5f5eff63130eac3a94724a2cd32
aw <aw@mega.co.nz>
2018-09-11 15:55:36 +1200

    dynamic calculate.

--- app/src/main/java/com/example/aw/animation/QRCodeActivity.java
+++ app/src/main/java/com/example/aw/animation/QRCodeActivity.java
@@ -4,6 +4,7 @@ package com.example.aw.animation;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.Build;
@@ -12,6 +13,7 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.example.aw.animation.util.QRCodeGenerator;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
@@ -41,26 +43,34 @@ public class QRCodeActivity extends Activity {
         qrImageView3 = findViewById(R.id.qr_2fa_3);
         try {
 //            gen1();
-//            gen2();
+            gen2();
             gen3();
         } catch (WriterException e) {
             e.printStackTrace();
         }
     }
     
-    public static final String URL = "otpauth://totp/Ash:aghghhhkkjkjkkl;lkklkl+1@ashtray.nz?secret=krqsus6fzz5evpidmqyozxwajfmfnqsgescvmh2eaykqcbxousuq&issuer=Ash";
+    public static final String URL = "otpauth://totp/Ash:a1312;saldflfdldasasddasdaskldasklasklkas@a.a?secret=krqsus6fzz5evpidmqyozxwajfmfnqsgescvmh2eaykqcbxousuq&issuer=Ash";
+//    public static final String URL = "d";
     
     public static final int CL = URL.length();
     
-    public static final int L = 700;
+    public static final int L = 500;
     
-    public static final int QR_L = 55;
+    public static final int QR_L = 50;
     
     public static final float SCALE = ((float)L / (float)QR_L);
     
     public static final float RADIUS = 5f;
     
-    private int x0 = -1, y0 = -1;
+    public static final float S_L_1 = 7f;
+    public static final float S_L_0 = S_L_1 + 1f;
+    public static final float S_L_2 = S_L_1 - 2f;
+    public static final float S_L_3 = S_L_2 - 2f;
+    public static final float S_L_4 = S_L_3 / 2f;
+    
+    private float x0 = -1, y0 = -1;
+    private float x1 = -1, y1 = -1;
     
     private void gen3() throws WriterException {
         long b = System.currentTimeMillis();
@@ -75,51 +85,64 @@ public class QRCodeActivity extends Activity {
         Paint paint = new Paint();
         paint.setAntiAlias(true);
         
-        paint.setColor(BLACK);
         for (int y = 0;y < QR_L;y++) {
             for (int x = 0;x < QR_L;x++) {
                 if (bitMatrix.get(x,y)) {
-//                    log(x +"/" + y);
                     if (x0 == -1 && y0 == -1) {
                         x0 = x;
                         y0 = y;
                     }
+                    if (y == x0) {
+                        x1 = x - S_L_1 + 1f;
+                    }
+                    if (x == y0) {
+                        y1 = y - S_L_1 + 1f;
+                    }
+                    paint.setColor(BLACK);
                     c.drawCircle(x * resize,y * resize,RADIUS,paint);
+                } else {
+                    paint.setColor(RED);
                 }
             }
         }
         drawRect(resize,c,paint);
         qrImageView1.setImageBitmap(qr);
-        log(x0 + "/" + y0);
+        log("0 = " + x0 + "/" + y0);
+        log("1 = " + x1 + "/" + y1);
         log("gen3:" + (System.currentTimeMillis() - b));
     }
     
     private void drawRect(float resize,Canvas c,Paint paint) {
+        paint.setColor(WHITE);
+        c.drawRect((x0 - 1f) * resize,(y0 - 1f) * resize,((x0 - 1f) + S_L_0) * resize,((y0 - 1f) + S_L_0) * resize,paint);
+        c.drawRect((x1 - 1f) * resize,(y0 - 1f) * resize,((x1 - 1f) + S_L_0) * resize,((y0 - 1f) + S_L_0) * resize,paint);
+        c.drawRect((x0 - 1f) * resize,(y1 - 1f) * resize,((x0 - 1f) + S_L_0) * resize,((y1 - 1f) + S_L_0) * resize,paint);
+        
         paint.setColor(BLACK);
         if (Build.VERSION.SDK_INT >= 21) {
-            c.drawRoundRect((x0 - 1) * resize,(y0 - 1) * resize,(x0 + 7) * resize,(y0 + 7) * resize,30,30,paint);
-            c.drawRoundRect(40f * resize,(y0 - 1) * resize,48f * resize,(y0 + 7) * resize,30,30,paint);
-            c.drawRoundRect((x0 - 1) * resize,(40f) * resize,(x0 + 7) * resize,48f * resize,30,30,paint);
+            c.drawRoundRect((x0 - 0.5f) * resize,(y0 - 0.5f) * resize,((x0 - 0.5f) + S_L_1) * resize,((y0 - 0.5f) + S_L_1) * resize,20,20,paint);
+            c.drawRoundRect((x1 - 0.5f) * resize,(y0 - 0.5f) * resize,((x1 - 0.5f) + S_L_1) * resize,((y0 - 0.5f) + S_L_1) * resize,20,20,paint);
+            c.drawRoundRect((x0 - 0.5f) * resize,(y1 - 0.5f) * resize,((x0 - 0.5f) + S_L_1) * resize,((y1 - 0.5f) + S_L_1) * resize,20,20,paint);
             
             paint.setColor(WHITE);
-            c.drawRoundRect((x0) * resize,(y0) * resize,(x0 + 6) * resize,(y0 + 6) * resize,25,25,paint);
-            c.drawRoundRect(41f * resize,(y0) * resize,47f * resize,(y0 + 6) * resize,25,25,paint);
-            c.drawRoundRect((x0) * resize,(41f) * resize,(x0 + 6) * resize,(47f) * resize,25,25,paint);
+            c.drawRoundRect((x0 + 0.5f) * resize,(y0 + 0.5f) * resize,((x0 + 0.5f) + S_L_2) * resize,((y0 + 0.5f) + S_L_2) * resize,15,15,paint);
+            c.drawRoundRect((x1 + 0.5f) * resize,(y0 + 0.5f) * resize,((x1 + 0.5f) + S_L_2) * resize,((y0 + 0.5f) + S_L_2) * resize,15,15,paint);
+            c.drawRoundRect((x0 + 0.5f) * resize,(y1 + 0.5f) * resize,((x0 + 0.5f) + S_L_2) * resize,((y1 + 0.5f) + S_L_2) * resize,15,15,paint);
         } else {
-            c.drawRoundRect(new RectF((x0 - 1) * resize,(y0 - 1) * resize,(x0 + 7) * resize,(y0 + 7) * resize),30,30,paint);
-            c.drawRoundRect(new RectF(40f * resize,6f * resize,48f * resize,14f * resize),30,30,paint);
-            c.drawRoundRect(new RectF((x0 - 1) * resize,(40f) * resize,(x0 + 7) * resize,48f * resize),30,30,paint);
+            c.drawRoundRect(new RectF((x0 - 0.5f) * resize,(y0 - 0.5f) * resize,((x0 - 0.5f) + S_L_1) * resize,((y0 - 0.5f) + S_L_1) * resize),30,30,paint);
+            c.drawRoundRect(new RectF((x1 - 0.5f) * resize,(y0 - 0.5f) * resize,((x1 - 0.5f) + S_L_1) * resize,((y0 - 0.5f) + S_L_1) * resize),30,30,paint);
+            c.drawRoundRect(new RectF((x0 - 0.5f) * resize,(y1 - 0.5f) * resize,((x0 - 0.5f) + S_L_1) * resize,((y1 - 0.5f) + S_L_1) * resize),30,30,paint);
             
             paint.setColor(WHITE);
-            c.drawRoundRect(new RectF((x0) * resize,(y0) * resize,(x0 + 6) * resize,(y0 + 6) * resize),25,25,paint);
-            c.drawRoundRect(new RectF(41f * resize,(y0) * resize,47f * resize,(y0 + 6) * resize),25,25,paint);
-            c.drawRoundRect(new RectF((x0) * resize,(41f) * resize,(x0 + 6) * resize,(47f) * resize),25,25,paint);
+            c.drawRoundRect(new RectF((x0 + 0.5f) * resize,(y0 + 0.5f) * resize,((x0 + 0.5f) + S_L_2) * resize,((y0 + 0.5f) + S_L_2) * resize),25,25,paint);
+            c.drawRoundRect(new RectF((x1 + 0.5f) * resize,(y0 + 0.5f) * resize,((x1 + 0.5f) + S_L_2) * resize,((y0 + 0.5f) + S_L_2) * resize),25,25,paint);
+            c.drawRoundRect(new RectF((x0 + 0.5f) * resize,(y1 + 0.5f) * resize,((x0 + 0.5f) + S_L_2) * resize,((y1 + 0.5f) + S_L_2) * resize),25,25,paint);
         }
         
         paint.setColor(BLACK);
-        c.drawCircle((x0 + 3) * resize,(y0 + 3) * resize,1.5f * resize,paint);
-        c.drawCircle(44f * resize,(y0 + 3) * resize,1.5f * resize,paint);
-        c.drawCircle((x0 + 3) * resize,44f * resize,1.5f * resize,paint);
+        c.drawCircle((x0 + S_L_3) * resize,(y0 + S_L_3) * resize,S_L_4 * resize,paint);
+        c.drawCircle((x1 + S_L_3) * resize,(y0 + S_L_3) * resize,S_L_4 * resize,paint);
+        c.drawCircle((x0 + S_L_3) * resize,(y1 + S_L_3) * resize,S_L_4 * resize,paint);
     }
     
     private Map<EncodeHintType, ErrorCorrectionLevel> getHints() {
@@ -129,47 +152,28 @@ public class QRCodeActivity extends Activity {
     }
     
     private void gen2() throws WriterException {
-        long b = System.currentTimeMillis();
-        BitMatrix bitMatrix = new MultiFormatWriter().encode(URL,BarcodeFormat.QR_CODE,QR_L,QR_L,null);
-        int w = bitMatrix.getWidth();
-        int h = bitMatrix.getHeight();
-        int[] pixels = new int[w * h];
-        float resize = SCALE;
-        Bitmap qr = Bitmap.createBitmap(L,L,Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(qr);
-        Paint paint = new Paint();
-        paint.setAntiAlias(true);
-        paint.setColor(WHITE);
-        c.drawRect(0,0,L,L,paint);
-        paint.setColor(BLACK);
+        QRCodeGenerator generator = new QRCodeGenerator();
+        generator.setDecorationColor(Color.RED);
+//        generator.setPointColor(Color.GREEN);
+        generator.setQrCodeSize(50);
+        generator.setPointRadius(5f);
         
-        for (int y = 0;y < h;y++) {
-            int offset = y * w;
-            for (int x = 0;x < w;x++) {
-                pixels[offset + x] = bitMatrix.get(x,y) ? BLACK : WHITE;
-                if (pixels[offset + x] == BLACK) {
-                    c.drawCircle(x * resize,y * resize,RADIUS,paint);
-                }
-            }
-        }
-//        drawRect(resize,c,paint);
-        qrImageView2.setImageBitmap(qr);
-        log("gen2:" + (System.currentTimeMillis() - b));
+        qrImageView2.setImageBitmap(generator.generate(URL));
     }
     
     private void gen1() throws WriterException {
-        BitMatrix encode = new MultiFormatWriter().encode(URL,BarcodeFormat.QR_CODE,QR_L,QR_L,null);
-        int[] pixels = new int[QR_L * QR_L];
-        for (int i = 0;i < QR_L;i++) {
-            for (int j = 0;j < QR_L;j++) {
+        BitMatrix encode = new MultiFormatWriter().encode(URL,BarcodeFormat.QR_CODE,500,500,null);
+        int[] pixels = new int[500 * 500];
+        for (int i = 0;i < 500;i++) {
+            for (int j = 0;j < 500;j++) {
                 if (encode.get(i,j)) {
-                    pixels[i * QR_L + j] = BLACK;
+                    pixels[i * 500 + j] = BLACK;
                 } else {
-                    pixels[i * QR_L + j] = WHITE;
+                    pixels[i * 500 + j] = WHITE;
                 }
             }
         }
-        Bitmap bitmap = Bitmap.createBitmap(pixels,0,QR_L,QR_L,QR_L,Bitmap.Config.RGB_565);
+        Bitmap bitmap = Bitmap.createBitmap(pixels,0,500,500,500,Bitmap.Config.RGB_565);
         qrImageView3.setImageBitmap(bitmap);
     }
     
---
+++ app/src/main/java/com/example/aw/animation/util/QRCodeGenerator.java
@@ -0,0 +1,170 @@
+package com.example.aw.animation.util;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.os.Build;
+import android.util.Log;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QRCodeGenerator {
+    
+    private float pointRadius = 5f;
+    
+    private int outerRadius = 20;
+    
+    private int innerRadius = 15;
+    
+    private int qrCodeSize = 60;
+    
+    private int canvasSize = 700;
+    
+    private int pointColor = Color.BLACK;
+    
+    private int decorationColor = Color.BLACK;
+    
+    private int bgColor = Color.WHITE;
+    
+    private boolean drawDecoration = true;
+    
+    private float x0 = -1f, y0 = -1f;
+    
+    private float x1, y1;
+    
+    private static final float SQUARE_SIZE = 7f;
+    
+    private static final float OUTER_SQUARE_SIZE = SQUARE_SIZE + 1f;
+    
+    private static final float INNER_SQUARE_SIZE = SQUARE_SIZE - 2f;
+    
+    private static final float CIRCLE_CENTRE = INNER_SQUARE_SIZE - 2f;
+    
+    private static final float CIRCLE_RADIUS = CIRCLE_CENTRE / 2f;
+    
+    private static final float TO_LEFT_OFFSET = -1f;
+    
+    private static final float TO_RIGHT_OFFSET = 1f;
+    
+    public Bitmap generate(String content) throws WriterException {
+        float resize = ((float)canvasSize / (float)qrCodeSize);
+        
+        BitMatrix bitMatrix = new MultiFormatWriter().encode(content,BarcodeFormat.QR_CODE,qrCodeSize,qrCodeSize,null);
+        Bitmap qr = Bitmap.createBitmap(canvasSize,canvasSize,Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(qr);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(pointColor);
+        for (int x = 0;x < qrCodeSize;x++) {
+            for (int y = 0;y < qrCodeSize;y++) {
+                if (bitMatrix.get(x,y)) {
+                    if (x0 == -1 && y0 == -1) {
+                        x0 = x;
+                        y0 = y;
+                    }
+                    if (y == x0) {
+                        x1 = x - SQUARE_SIZE + 1f;
+                    }
+                    if (x == y0) {
+                        y1 = y - SQUARE_SIZE + 1f;
+                    }
+                    c.drawCircle(x * resize,y * resize,pointRadius,paint);
+                }
+            }
+        }
+        if (drawDecoration) {
+            drawRect(resize,c,paint);
+        }
+        return qr;
+    }
+    
+    private Map<EncodeHintType, ErrorCorrectionLevel> getHints() {
+        Map<EncodeHintType, ErrorCorrectionLevel> hints = new HashMap<>();
+        hints.put(EncodeHintType.ERROR_CORRECTION,ErrorCorrectionLevel.H);
+        return hints;
+    }
+    
+    private void drawRect(float resize,Canvas c,Paint paint) {
+        paint.setColor(bgColor);
+        c.drawRect((x0 + TO_LEFT_OFFSET) * resize,(y0 + TO_LEFT_OFFSET) * resize,((x0 + TO_LEFT_OFFSET) + OUTER_SQUARE_SIZE) * resize,((y0 + TO_LEFT_OFFSET) + OUTER_SQUARE_SIZE) * resize,paint);
+        c.drawRect((x1 + TO_LEFT_OFFSET) * resize,(y0 + TO_LEFT_OFFSET) * resize,((x1 + TO_LEFT_OFFSET) + OUTER_SQUARE_SIZE) * resize,((y0 + TO_LEFT_OFFSET) + OUTER_SQUARE_SIZE) * resize,paint);
+        c.drawRect((x0 + TO_LEFT_OFFSET) * resize,(y1 + TO_LEFT_OFFSET) * resize,((x0 + TO_LEFT_OFFSET) + OUTER_SQUARE_SIZE) * resize,((y1 + TO_LEFT_OFFSET) + OUTER_SQUARE_SIZE) * resize,paint);
+        
+        paint.setColor(decorationColor);
+        if (Build.VERSION.SDK_INT >= 21) {
+            c.drawRoundRect((x0 + TO_LEFT_OFFSET / 2) * resize,(y0 + TO_LEFT_OFFSET / 2) * resize,((x0 + TO_LEFT_OFFSET / 2) + SQUARE_SIZE) * resize,((y0 + TO_LEFT_OFFSET / 2) + SQUARE_SIZE) * resize,outerRadius,outerRadius,paint);
+            c.drawRoundRect((x1 + TO_LEFT_OFFSET / 2) * resize,(y0 + TO_LEFT_OFFSET / 2) * resize,((x1 + TO_LEFT_OFFSET / 2) + SQUARE_SIZE) * resize,((y0 + TO_LEFT_OFFSET / 2) + SQUARE_SIZE) * resize,outerRadius,outerRadius,paint);
+            c.drawRoundRect((x0 + TO_LEFT_OFFSET / 2) * resize,(y1 + TO_LEFT_OFFSET / 2) * resize,((x0 + TO_LEFT_OFFSET / 2) + SQUARE_SIZE) * resize,((y1 + TO_LEFT_OFFSET / 2) + SQUARE_SIZE) * resize,outerRadius,outerRadius,paint);
+            
+            paint.setColor(bgColor);
+            c.drawRoundRect((x0 + TO_RIGHT_OFFSET / 2) * resize,(y0 + TO_RIGHT_OFFSET / 2) * resize,((x0 + TO_RIGHT_OFFSET / 2) + INNER_SQUARE_SIZE) * resize,((y0 + TO_RIGHT_OFFSET / 2) + INNER_SQUARE_SIZE) * resize,innerRadius,innerRadius,paint);
+            c.drawRoundRect((x1 + TO_RIGHT_OFFSET / 2) * resize,(y0 + TO_RIGHT_OFFSET / 2) * resize,((x1 + TO_RIGHT_OFFSET / 2) + INNER_SQUARE_SIZE) * resize,((y0 + TO_RIGHT_OFFSET / 2) + INNER_SQUARE_SIZE) * resize,innerRadius,innerRadius,paint);
+            c.drawRoundRect((x0 + TO_RIGHT_OFFSET / 2) * resize,(y1 + TO_RIGHT_OFFSET / 2) * resize,((x0 + TO_RIGHT_OFFSET / 2) + INNER_SQUARE_SIZE) * resize,((y1 + TO_RIGHT_OFFSET / 2) + INNER_SQUARE_SIZE) * resize,innerRadius,innerRadius,paint);
+        } else {
+            c.drawRoundRect(new RectF((x0 + TO_LEFT_OFFSET / 2) * resize,(y0 + TO_LEFT_OFFSET / 2) * resize,((x0 + TO_LEFT_OFFSET / 2) + SQUARE_SIZE) * resize,((y0 + TO_LEFT_OFFSET / 2) + SQUARE_SIZE) * resize),outerRadius,outerRadius,paint);
+            c.drawRoundRect(new RectF((x1 + TO_LEFT_OFFSET / 2) * resize,(y0 + TO_LEFT_OFFSET / 2) * resize,((x1 + TO_LEFT_OFFSET / 2) + SQUARE_SIZE) * resize,((y0 + TO_LEFT_OFFSET / 2) + SQUARE_SIZE) * resize),outerRadius,outerRadius,paint);
+            c.drawRoundRect(new RectF((x0 + TO_LEFT_OFFSET / 2) * resize,(y1 + TO_LEFT_OFFSET / 2) * resize,((x0 + TO_LEFT_OFFSET / 2) + SQUARE_SIZE) * resize,((y1 + TO_LEFT_OFFSET / 2) + SQUARE_SIZE) * resize),outerRadius,outerRadius,paint);
+            
+            paint.setColor(bgColor);
+            c.drawRoundRect(new RectF((x0 + TO_RIGHT_OFFSET / 2) * resize,(y0 + TO_RIGHT_OFFSET / 2) * resize,((x0 + TO_RIGHT_OFFSET / 2) + INNER_SQUARE_SIZE) * resize,((y0 + TO_RIGHT_OFFSET / 2) + INNER_SQUARE_SIZE) * resize),innerRadius,innerRadius,paint);
+            c.drawRoundRect(new RectF((x1 + TO_RIGHT_OFFSET / 2) * resize,(y0 + TO_RIGHT_OFFSET / 2) * resize,((x1 + TO_RIGHT_OFFSET / 2) + INNER_SQUARE_SIZE) * resize,((y0 + TO_RIGHT_OFFSET / 2) + INNER_SQUARE_SIZE) * resize),innerRadius,innerRadius,paint);
+            c.drawRoundRect(new RectF((x0 + TO_RIGHT_OFFSET / 2) * resize,(y1 + TO_RIGHT_OFFSET / 2) * resize,((x0 + TO_RIGHT_OFFSET / 2) + INNER_SQUARE_SIZE) * resize,((y1 + TO_RIGHT_OFFSET / 2) + INNER_SQUARE_SIZE) * resize),innerRadius,innerRadius,paint);
+        }
+        
+        paint.setColor(decorationColor);
+        c.drawCircle((x0 + CIRCLE_CENTRE) * resize,(y0 + CIRCLE_CENTRE) * resize,CIRCLE_RADIUS * resize,paint);
+        c.drawCircle((x1 + CIRCLE_CENTRE) * resize,(y0 + CIRCLE_CENTRE) * resize,CIRCLE_RADIUS * resize,paint);
+        c.drawCircle((x0 + CIRCLE_CENTRE) * resize,(y1 + CIRCLE_CENTRE) * resize,CIRCLE_RADIUS * resize,paint);
+    }
+    
+    public void setPointRadius(float pointRadius) {
+        this.pointRadius = pointRadius;
+    }
+    
+    public void setOuterRadius(int outerRadius) {
+        this.outerRadius = outerRadius;
+    }
+    
+    public void setInnerRadius(int innerRadius) {
+        this.innerRadius = innerRadius;
+    }
+    
+    public void setQrCodeSize(int qrCodeSize) {
+        this.qrCodeSize = qrCodeSize;
+    }
+    
+    public void setCanvasSize(int canvasSize) {
+        this.canvasSize = canvasSize;
+    }
+    
+    public void setPointColor(int pointColor) {
+        this.pointColor = pointColor;
+    }
+    
+    public void setDecorationColor(int decorationColor) {
+        this.decorationColor = decorationColor;
+    }
+    
+    public void setBgColor(int bgColor) {
+        this.bgColor = bgColor;
+    }
+    
+    public void setDrawDecoration(boolean drawDecoration) {
+        this.drawDecoration = drawDecoration;
+    }
+    
+    private void log(Object anything) {
+        String msg = (anything == null) ? "NULL" : anything.toString();
+        Log.e("@#@"," ---> :" + msg);
+    }
+}
